7f5c9e4661117de0dd7b4cec3f35cacd32221f46
Bethany Tormino <torminobethany@gmail.com>
2023-01-16 21:20:57 -0600

    This is all Chamane's work. I was able to take the information from her original pull request and get it in a new branch. It seems to be working.

---
+++ BucketListAdventures/Views/Home/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BucketListAdventures.Views.Home
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
