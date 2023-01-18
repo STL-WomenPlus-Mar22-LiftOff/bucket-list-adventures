7ac85666f8b3dbe7a8f3c8e8789b08c85c51b701
Brigid <brigidbook@gmail.com>
2023-01-18 16:53:15 -0600

    created weather station class

---
+++ BucketListAdventures/Models/WeatherStation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Spatial;
+
+namespace BucketListAdventures.Models
+{
+    public class WeatherStation
+    {
+        //This is the primary key
+        [Key]
+        public int primary_id { get; set; }
+        //This is the id that NOAA uses for the weather station
+        public string station_id { get; set; }
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        //elevation is in meters
+        public double elevation { get; set; }
+        public string station_name { get; set; }
+        public GeographyPoint geography_point => GeographyPoint.Create(latitude, longitude);
+    }
+}
