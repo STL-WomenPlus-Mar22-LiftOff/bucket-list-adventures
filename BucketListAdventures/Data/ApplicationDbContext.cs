161cdbbca7bd9acba918a7f5c39635a5836e70a2
Brigid <brigidbook@gmail.com>
2023-01-18 16:54:10 -0600

    Added weather stations to dbcontext

--- BucketListAdventures/Data/ApplicationDbContext.cs
+++ BucketListAdventures/Data/ApplicationDbContext.cs
@@ -1,10 +1,18 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BucketListAdventures.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BucketListAdventures.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<WeatherStation> WeatherStations { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //this property is not in the table and is only created in the class
+            //so it should be excluded from mapping
+            modelBuilder.Entity<WeatherStation>().Ignore(x => x.geography_point);
+        }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
