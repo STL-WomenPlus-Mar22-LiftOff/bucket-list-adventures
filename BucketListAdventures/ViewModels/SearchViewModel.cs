0621bba5248cd7eb78ff53fa4163eb8b10e76815
Brigid <brigidbook@gmail.com>
2023-01-23 21:29:11 -0600

    Autocomplete field not showing up, don't know why

--- BucketListAdventures/ViewModels/SearchViewModel.cs
+++ BucketListAdventures/ViewModels/SearchViewModel.cs
@@ -3,7 +3,7 @@
     public class SearchViewModel
     {
         public string CityName { get; set; }
-       
+        public string ApiKey { get; } = "8bf27d74b51c4680a953442ad9dab614";
 
     }
 
